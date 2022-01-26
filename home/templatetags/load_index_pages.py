155e1a3fa8606f0469383ce1e1e3ea56c28da59a
MEGAN RYDER <megsryder@outlook.com>
2022-01-26 14:45:14 +0200

    creating of navbar links

--- home/templatetags/load_index_pages.py
+++ home/templatetags/load_index_pages.py
@@ -1,10 +1,19 @@
 from django import template
+from gene_details.models import GeneDetailsIndexPage
 from studies.models import StudiesIndexPage
+from variant_details.models import VariantDetailsIndexPage
 
 register = template.Library()
 
 
 class LoadIndexPages(template.Node):
-    def Render(self, context):
+    def render(self, context):
         context["studies_page"] = StudiesIndexPage.objects.get()
+        context["variants_page"] = VariantDetailsIndexPage.objects.get()
+        context["genes_page"] = GeneDetailsIndexPage.objects.get()
         return ""
+
+
+@register.tag
+def load_page(self, request):
+    return LoadIndexPages()
